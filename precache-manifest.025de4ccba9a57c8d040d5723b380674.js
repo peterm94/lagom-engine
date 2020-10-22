d01e0bf5453f5aeab342ab0152cbabc1e59da8fd
Peter Mandile <peterm1994@hotmail.com>
2020-10-23 01:33:28 +1100

    Updates

---
+++ precache-manifest.025de4ccba9a57c8d040d5723b380674.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50dad1e911dd4e2bdcfd81672ca0445f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c057a8d127e03e0b151",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "7d13d1310fad13725c65",
+    "url": "./static/js/2.3783065a.chunk.js"
+  },
+  {
+    "revision": "26845192d0cd9752190627f5124406c5",
+    "url": "./static/js/2.3783065a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c057a8d127e03e0b151",
+    "url": "./static/js/main.1167ef64.chunk.js"
+  },
+  {
+    "revision": "7fc70482fce0cbbc19350fb384250c02",
+    "url": "./static/js/main.1167ef64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c909596d6094cd874a",
+    "url": "./static/js/runtime-main.bea0e03e.js"
+  }
+]);
