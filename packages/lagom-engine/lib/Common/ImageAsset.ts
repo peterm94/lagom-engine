2e90326b991d13535611743716d9f8b3f9cbcbd3
Peter Mandile <peterm1994@hotmail.com>
2026-02-04 20:22:52 +1100

    finished texture loader

--- packages/lagom-engine/lib/Common/ImageAsset.ts
+++ packages/lagom-engine/lib/Common/ImageAsset.ts
@@ -1,26 +1,54 @@
-import {Assets, Rectangle, Texture, TextureSource} from "pixi.js";
+import {Rectangle, Texture, TextureSource} from "pixi.js";
+import {Sprite, SpriteConfig} from "./Sprite/Sprite";
 
 export interface AssetOptions {
     tileWidth: number;
     tileHeight: number;
 }
 
+export class LTexture extends Texture {
+    sprite(config?: SpriteConfig): Sprite {
+        return new Sprite(this, config);
+    }
+}
+
 export class ImageAsset {
 
-    constructor(readonly alias: string, readonly texture: TextureSource, readonly options?: AssetOptions) {
-    }
+    private readonly cols: number;
+    private readonly rows: number;
+    private readonly tileWidth: number;
+    private readonly tileHeight: number;
 
-    tileAt(column: number, row: number): Texture {
-        if (this.options === undefined) {
-            throw new Error("Image Asset does not have defined tile settings")
+    constructor(readonly alias: string, readonly texture: TextureSource, options?: AssetOptions) {
+        this.cols = options?.tileWidth ? texture.width / options.tileWidth : 1;
+        this.rows = options?.tileHeight ? texture.height / options.tileHeight : 1;
+
+        // Validate we divide cleanly
+        if (!Number.isInteger(this.cols) || !Number.isInteger(this.rows)) {
+            throw new Error(`Sprite '${alias} has incorrect tile dimensions.`)
         }
 
-        return new Texture({
-            source: this.texture, frame: new Rectangle(column * this.options.tileWidth,
-                row * this.options.tileHeight,
-                this.options.tileWidth, this.options.tileHeight)
-        });
+        this.tileWidth = options?.tileWidth ?? texture.width;
+        this.tileHeight = options?.tileHeight ?? texture.height;
     }
 
+    tileAt(column: number, row: number): LTexture {
+        return this.fromPoints(column * this.tileWidth, row * this.tileHeight, this.tileWidth, this.tileHeight);
+    }
 
+    fromPoints(x: number, y: number, w: number, h: number): LTexture {
+        return new LTexture({source: this.texture, frame: new Rectangle(x, y, w, h)});
+    }
+
+    tileIdx(index: number): LTexture {
+        return this.tileAt(index % this.cols, Math.floor(index / this.rows));
+    }
+
+    tileSlice(firstIdx: number, lastIdx: number): LTexture[] {
+        return Array.from({length: lastIdx - firstIdx + 1}, (_, i) => this.tileIdx(firstIdx + i))
+    }
+
+    allTiles(): LTexture[] {
+        return this.tileSlice(0, this.rows * this.cols);
+    }
 }
